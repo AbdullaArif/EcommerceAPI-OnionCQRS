2b700b29de2addf3f48b713f807825579556057d
AbdullaArif <arifabdulla034@gmail.com>
2025-01-10 00:02:44 +0400

    Read Repository

---
+++ Core/EcommerceAPI.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using EcommerceAPI.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Interfaces.Repositories
+{
+    public interface IWriteRepository<T> where T : class,IEntityBase,new()
+    {
+
+    }
+}
--- Infrastructure/EcommerceAPI.Persistance/Registration.cs
+++ Infrastructure/EcommerceAPI.Persistance/Registration.cs
@@ -1,4 +1,6 @@
-﻿using EcommerceAPI.Persistance.Context;
+﻿using EcommerceAPI.Application.Interfaces.Repositories;
+using EcommerceAPI.Persistance.Context;
+using EcommerceAPI.Persistance.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,7 +17,13 @@ namespace EcommerceAPI.Persistance
         public static void AddPersistence(this IServiceCollection services,IConfiguration configuration)
         {
             services.AddDbContext<AppDbContext>(opt =>
+
      opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+
+            services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+
+
         }
         
 
