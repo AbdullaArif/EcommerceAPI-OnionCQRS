ff1b12dceedc1ac5ab87edb436cd5a3da742f63d
AbdullaArif <arifabdulla034@gmail.com>
2025-01-09 19:50:00 +0400

    DbContext Created

---
+++ Infrastructure/EcommerceAPI.Persistance/Context/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using EcommerceAPI.Domain.Entites;
+using EcommerceAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Persistance.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Product> Products { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Brand> Brands { get; set; }
+        DbSet<Detail> Details { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
