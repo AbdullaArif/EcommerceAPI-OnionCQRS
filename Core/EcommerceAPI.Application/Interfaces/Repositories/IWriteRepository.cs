43ea25c5ee640faf7273ba55e9b8dd5a3ef9df84
AbdullaArif <arifabdulla034@gmail.com>
2025-01-10 22:06:49 +0400

    Write Repository

--- Core/EcommerceAPI.Application/Interfaces/Repositories/IWriteRepository.cs
+++ Core/EcommerceAPI.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -9,6 +9,13 @@ namespace EcommerceAPI.Application.Interfaces.Repositories
 {
     public interface IWriteRepository<T> where T : class,IEntityBase,new()
     {
+        Task AddAsync(T entity);
 
+        Task AddRangeAsync(IList<T> entities);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task HardDeleteAsync(T entity);  
+        
     }
 }
