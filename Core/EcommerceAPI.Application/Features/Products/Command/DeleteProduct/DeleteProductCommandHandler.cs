4584892dacd667b5c08536c36426145d383d2a4e
AbdullaArif <arifabdulla034@gmail.com>
2025-01-13 23:33:01 +0400

    Error handling Product CRUD success

--- Core/EcommerceAPI.Application/Features/Products/Command/DeleteProduct/DeleteProductCommandHandler.cs
+++ Core/EcommerceAPI.Application/Features/Products/Command/DeleteProduct/DeleteProductCommandHandler.cs
@@ -12,10 +12,17 @@ namespace EcommerceAPI.Application.Features.Products.Command.DeleteProduct
     public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommandRequest>
     {
         private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteProductCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public async Task Handle(DeleteProductCommandRequest request, CancellationToken cancellationToken)
         {
             var product = await _unitOfWork.GetReadRepository<Product>()
                 .GetAsync(p=> p.Id == request.Id && !p.IsDeleted);
+           
             product.IsDeleted = true;
 
             await _unitOfWork.GetWriteRepository<Product>().UpdateAsync(product);
