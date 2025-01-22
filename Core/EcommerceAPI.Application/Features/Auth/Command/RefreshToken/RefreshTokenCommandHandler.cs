d389e42a432a3a86203f96cdfa74769b00da4f0b
AbdullaArif <arifabdulla034@gmail.com>
2025-01-23 01:05:42 +0400

    Refresh Token  Command done :)

---
+++ Core/EcommerceAPI.Application/Features/Auth/Command/RefreshToken/RefreshTokenCommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using EcommerceAPI.Application.Bases;
+using EcommerceAPI.Application.Features.Auth.Rules;
+using EcommerceAPI.Application.Interfaces.AutoMapper;
+using EcommerceAPI.Application.Interfaces.Tokens;
+using EcommerceAPI.Application.Interfaces.UnitOfWorks;
+using EcommerceAPI.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Features.Auth.Command.RefreshToken
+{
+    public class RefreshTokenCommandHandler : BaseHandler, IRequestHandler<RefreshTokenCommandRequest, RefreshTokenCommandResponse>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ITokenService _tokenService;
+        private readonly AuthRules _authRules;
+
+        public RefreshTokenCommandHandler(UserManager<User> userManager, ITokenService tokenService, 
+            AuthRules authRules,IUnitOfWork unitOfWork, IMapper mapper, IHttpContextAccessor contextAccessor) 
+            : base(unitOfWork, mapper, contextAccessor)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+            _authRules = authRules;
+        }
+
+        public async Task<RefreshTokenCommandResponse> Handle(RefreshTokenCommandRequest request, CancellationToken cancellationToken)
+        {
+            ClaimsPrincipal? principal = _tokenService.GetPrincipalFromExpiredToken(request.AccessToken);
+            string? email = principal.FindFirstValue(ClaimTypes.Email);
+
+            User? user = await _userManager.FindByEmailAsync(email);
+
+            IList<string> roles =await _userManager.GetRolesAsync(user);
+
+            await _authRules.RefreshTokenShouldNottBeExpired(user.RefreshTokenExpriyTime);
+
+            JwtSecurityToken newAccesToken = await _tokenService.CreateToken(user, roles);
+            string newRefreshToken = _tokenService.GenerateRefreshToken();
+            user.RefreshToken = newRefreshToken;
+            await _userManager.UpdateAsync(user);
+
+            return new()
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(newAccesToken),
+                RefreshToken = newRefreshToken
+            };
+
+
+        }
+    }
+}
