913efeacaa32384610d482e50d909ed98cace71a
AbdullaArif <arifabdulla034@gmail.com>
2025-01-27 02:04:32 +0400

    Get By Id for Brand

---
+++ Core/EcommerceAPI.Application/Features/Brands/Queries/GetByIdBrand/GetByIdBrandQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using EcommerceAPI.Application.Interfaces.AutoMapper;
+using EcommerceAPI.Application.Interfaces.UnitOfWorks;
+using EcommerceAPI.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Features.Brands.Queries.GetByIdBrand
+{
+    public class GetByIdBrandQueryHandler : IRequestHandler<GetByIdBrandQueryRequest, GetByIdBrandQueryResponse>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetByIdBrandQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<GetByIdBrandQueryResponse> Handle(GetByIdBrandQueryRequest request, CancellationToken cancellationToken)
+        {
+            var brand = await _unitOfWork.GetReadRepository<Brand>().GetAsync(b => b.Id == request.Id && !b.IsDeleted);
+
+            var map = _mapper.Map<GetByIdBrandQueryResponse, Brand>(brand);
+
+            return map;
+        }
+    }
+}
---
+++ Core/EcommerceAPI.Application/Features/Brands/Queries/GetByIdBrand/GetByIdBrandQueryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Features.Brands.Queries.GetByIdBrand
+{
+    public class GetByIdBrandQueryResponse
+    {
+        public string Name { get; set; }
+
+    }
+}
