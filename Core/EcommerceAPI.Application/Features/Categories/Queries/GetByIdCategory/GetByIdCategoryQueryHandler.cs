636c455e1e76af8beb1adb697471743e9d3dcfe1
AbdullaArif <arifabdulla034@gmail.com>
2025-01-27 01:53:56 +0400

    Get By Id for Category

---
+++ Core/EcommerceAPI.Application/Features/Categories/Queries/GetByIdCategory/GetByIdCategoryQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using EcommerceAPI.Application.DTOs;
+using EcommerceAPI.Application.Interfaces.AutoMapper;
+using EcommerceAPI.Application.Interfaces.UnitOfWorks;
+using EcommerceAPI.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Features.Categories.Queries.GetByIdCategory
+{
+    public class GetByIdCategoryQueryHandler : IRequestHandler<GetByIdCategoryQueryRequest, GetByIdCategoryQueryResponse>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetByIdCategoryQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<GetByIdCategoryQueryResponse> Handle(GetByIdCategoryQueryRequest request, CancellationToken cancellationToken)
+        {
+            var category = await _unitOfWork.GetReadRepository<Category>().GetAsync(c => c.Id == request.Id && !c.IsDeleted,
+                include: x => x.Include(d => d.Details));
+            DetailDto details = _mapper.Map<DetailDto, Detail>(new Detail());
+
+            var map = _mapper.Map<GetByIdCategoryQueryResponse,Category>(category);
+
+            return map;
+        }
+    }
+}
---
+++ Core/EcommerceAPI.Application/Features/Categories/Queries/GetByIdCategory/GetByIdCategoryQueryResponse.cs
@@ -0,0 +1,19 @@
+﻿using EcommerceAPI.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Features.Categories.Queries.GetByIdCategory
+{
+    public class GetByIdCategoryQueryResponse
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+        public int Priorty { get; set; }
+
+        public IList<DetailDto> Details { get; set; }
+    }
+}
