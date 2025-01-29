10995a63aea7239a2a0bd1085921adbc5e07559f
AbdullaArif <arifabdulla034@gmail.com>
2025-01-29 18:11:13 +0400

    Brand Rules

---
+++ Core/EcommerceAPI.Application/Features/Brands/Exceptions/BrandMustBeUniqueException.cs
@@ -0,0 +1,17 @@
+﻿using EcommerceAPI.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Features.Brands.Exceptions
+{
+    public class BrandMustBeUniqueException:BaseException
+    {
+        public BrandMustBeUniqueException():base("Brand adi unikal olmalidir")
+        {
+        }
+
+    }
+}
