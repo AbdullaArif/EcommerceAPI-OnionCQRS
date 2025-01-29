4a9b46512a49d91f4696f57bd97e209429154012
AbdullaArif <arifabdulla034@gmail.com>
2025-01-29 18:04:10 +0400

    Category and Detail Rules

---
+++ Core/EcommerceAPI.Application/Features/Categories/Exceptions/CategoryMustBeUniqueException.cs
@@ -0,0 +1,16 @@
+﻿using EcommerceAPI.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Features.Categories.Exceptions
+{
+    public class CategoryMustBeUniqueException:BaseException
+    {
+        public CategoryMustBeUniqueException():base("Category Name unikal olmalidir")
+        {    
+        }
+    }
+}
---
+++ Core/EcommerceAPI.Application/Features/Details/Rules/DetailRules.cs
@@ -0,0 +1,14 @@
+﻿using EcommerceAPI.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Features.Details.Rules
+{
+    public class DetailRules: BaseRules
+    {
+        
+    }
+}
