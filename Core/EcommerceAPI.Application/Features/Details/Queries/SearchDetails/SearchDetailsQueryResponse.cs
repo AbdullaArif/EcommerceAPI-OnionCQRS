ce11a241438c2c2f1140ea861f75b04159d0a48c
AbdullaArif <arifabdulla034@gmail.com>
2025-02-03 23:29:25 +0400

    Searching Done for Details

---
+++ Core/EcommerceAPI.Application/Features/Details/Queries/SearchDetails/SearchDetailsQueryResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Application.Features.Details.Queries.SearchDetails
+{
+    public class SearchDetailsQueryResponse
+    {
+        public string Tittle { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
